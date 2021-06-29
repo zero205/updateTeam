77007cc0428584529e3337b6f87d131d541e1f53
zero205 <1184642956@qq.com>
2021-06-30 00:37:34 +0800

    Update jd_updateRed.js

--- jd_updateRed.js
+++ jd_updateRed.js
@@ -1944,7 +1944,6 @@ function jsonParse(str) {
 }(this);
 
 function Env(t, e) {
-    "undefined" != typeof process && JSON.stringify(process.env).indexOf("GITHUB") > -1 && process.exit(0);
     class s {
         constructor(t) {
             this.env = t
