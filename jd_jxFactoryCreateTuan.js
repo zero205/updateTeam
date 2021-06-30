dfc1a7e854d50d7774d137408f1bcb48c3ac98c5
zero205 <1184642956@qq.com>
2021-07-01 07:51:46 +0800

    Update jd_jxFactoryCreateTuan.js

--- jd_jxFactoryCreateTuan.js
+++ jd_jxFactoryCreateTuan.js
@@ -2,7 +2,7 @@
 京东京喜工厂自动开团
  */
 const $ = new Env('京东京喜工厂自动开团');
-const tuanActiveId = `bozIUUFcANuUdWpw3QdvPw==`;
+const tuanActiveId = `T_zZaWP6by9yA1wehxM4mg==`;
 const JD_API_HOST = 'https://m.jingxi.com';
 const fs = require('fs');
 const notify = $.isNode() ? require('./sendNotify') : '';
